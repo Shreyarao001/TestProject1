2715509d5ccbbc711e24fb595aa791a57e98094a
shreya <shreyachinmayi@gmail.com>
2024-02-21 19:18:45 +0530

    Add project files.

---
+++ TestProject1/StudentTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using ClassLibrary1;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject1
+{
+    public class StudentTest
+    {
+        [Test]
+        public void TestStudentName()
+        {
+            // Arrange
+            Student student = new Student();
+            student.Name = "Preethi";
+            // Act
+            string studentName = student.Name;
+            // Assert
+            Assert.AreEqual("Preethi", studentName);
+        }
+        [Test]
+        public void TestStudentAddress()
+        {
+            // Arrange
+            Student student = new Student();
+            student.Address = "yusufguda";
+            // Act
+            string studentAddress = student.Address;
+            // Assert
+            Assert.AreEqual("yusufguda", studentAddress);
+        }
+    }
+}
---
+++ TestProject1/TeacherTest.cs
@@ -0,0 +1,35 @@
+﻿using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject1
+{
+    internal class TeacherTest
+    {
+        [Test]
+        public void TestTeacherName()
+        {
+            // Arrange
+            Teacher myTeacher = new Teacher();
+            myTeacher.Name = "Suresh";
+            // Act
+            string teacherName = myTeacher.Name;
+            // Assert
+            Assert.AreEqual("Suresh", teacherName);
+        }
+        [Test]
+        public void TestClassId()
+        {
+            // Arrange
+            Teacher myTeacher = new Teacher();
+            myTeacher.Id = 1;
+            // Act
+            int tecaherId = myTeacher.Id;
+            // Assert
+            Assert.AreEqual(1, tecaherId);
+        }
+    }
+}
